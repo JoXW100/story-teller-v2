3afe772a3902c84e613565f2a2b6ae2eea0ab642
JoXW100 <joxw100@gmail.com>
2023-01-03 18:01:07 +0100

    Resolved dependacy issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps, router }) {
                 <meta key="og:title" property="og:title" content="Story Teller 2"/>
                 <meta key="og:description" property="og:description" content="Create your own story!"/>
                 <meta key="og:type" property="og:type" content="website" />
-                <meta key="og:image" property="og:image" content="./storyteller.png"/>
+                <meta key="og:image" property="og:image" content="/storyteller.png"/>
                 <meta key="twitter:card" property="twitter:card" content="summary"/>
             </Head>
             <AppContext>
