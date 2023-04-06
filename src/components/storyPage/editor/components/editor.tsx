411ab44aa6764a4f0ed5bbb6a22516b3b67fb928
JoXW100 <joxw100@gmail.com>
2023-04-06 17:06:31 +0200

    Added more commands to editor context menu

--- src/components/storyPage/editor/components/editor.tsx
+++ src/components/storyPage/editor/components/editor.tsx
@@ -1,7 +1,4 @@
 import React, { useContext } from 'react'
-import { Context } from 'components/contexts/fileContext';
-import { openContext } from 'components/common/contextMenu';
-import TextEditor from 'components/common/textEditor';
 import EditIcon from '@mui/icons-material/EditSharp';
 import AlignIcon from '@mui/icons-material/AlignHorizontalLeftSharp';
 import BoxIcon from '@mui/icons-material/CheckBoxOutlineBlankSharp';
@@ -10,10 +7,21 @@ import PasteIcon from '@mui/icons-material/ContentPasteSharp';
 import BlockIcon from '@mui/icons-material/GridViewSharp';
 import BoldIcon from '@mui/icons-material/FormatBoldSharp';
 import LinkIcon from '@mui/icons-material/InsertLinkSharp';
+import LayoutIcon from '@mui/icons-material/DashboardSharp';
+import InteractiveIcon from '@mui/icons-material/TouchAppSharp';
+import CenterIcon from '@mui/icons-material/FilterCenterFocusSharp';
+import DiceIcon from '@mui/icons-material/CasinoSharp';
+import LineIcon from '@mui/icons-material/HorizontalRuleSharp';
+import SpaceIcon from '@mui/icons-material/SpaceBarSharp';
+import IconIcon from '@mui/icons-material/InsertEmoticonSharp';
+import ImageIcon from '@mui/icons-material/InsertPhotoSharp';
+import TextIcon from '@mui/icons-material/TextFieldsSharp';
+import { Context } from 'components/contexts/fileContext';
+import { openContext } from 'components/common/contextMenu';
+import TextEditor from 'components/common/textEditor';
 import Localization from 'utils/localization';
 import { TemplateComponentProps } from '.';
 import { FileTemplateParams } from 'types/templates';
-import styles from 'styles/pages/storyPage/editor.module.scss'
 
 const EditorComponent = ({}: TemplateComponentProps<FileTemplateParams>): JSX.Element => {
     const [context, dispatch] = useContext(Context)
@@ -41,39 +49,107 @@ const EditorComponent = ({}: TemplateComponentProps<FileTemplateParams>): JSX.El
                 icon: EditIcon,
                 content: [
                     {
-                        text: Localization.toText('editor-insert-align'), 
-                        icon: AlignIcon, 
-                        action: () => insertText(target, `\\align[hc] {${selection}}`),
+                        text: Localization.toText('editor-insert-layout'),
+                        icon: LayoutIcon,
+                        content: [
+                            {
+                                text: Localization.toText('editor-insert-align'), 
+                                icon: AlignIcon, 
+                                action: () => insertText(target, `\\align[hc] {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-block'), 
+                                icon: BlockIcon, 
+                                action: () => insertText(target, `\\block {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-box'), 
+                                icon: BoxIcon, 
+                                action: () => insertText(target, `\\box {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-center'), 
+                                icon: CenterIcon, 
+                                action: () => insertText(target, `\\center {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-line'), 
+                                icon: LineIcon, 
+                                action: () => insertText(target, `\\line`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-space'), 
+                                icon: SpaceIcon, 
+                                action: () => insertText(target, `\\space`),
+                            }
+                        ]
                     },
                     {
-                        text: Localization.toText('editor-insert-block'), 
-                        icon: BlockIcon, 
-                        action: () => insertText(target, `\\block {${selection}}`),
+                        text: Localization.toText('editor-insert-decoration'),
+                        icon: ImageIcon,
+                        content: [
+                            {
+                                text: Localization.toText('editor-insert-bold'), 
+                                icon: BoldIcon, 
+                                action: () => insertText(target, `\\bold {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-header'), 
+                                icon: TextIcon, 
+                                action: () => insertText(target, `\\h1 {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-subheader'), 
+                                icon: TextIcon, 
+                                action: () => insertText(target, `\\h2 {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-subsubheader'), 
+                                icon: TextIcon, 
+                                action: () => insertText(target, `\\h3 {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-icon'), 
+                                icon: IconIcon, 
+                                action: () => insertText(target, `\\icon[acid, tooltips: Acid]`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-image'), 
+                                icon: ImageIcon, 
+                                action: () => insertText(target, `\\image[]`),
+                            }
+                        ]
                     },
                     {
-                        text: Localization.toText('editor-insert-bold'), 
-                        icon: BoldIcon, 
-                        action: () => insertText(target, `\\bold {${selection}}`),
-                    },
-                    {
-                        text: Localization.toText('editor-insert-box'), 
-                        icon: BoxIcon, 
-                        action: () => insertText(target, `\\box[true] {${selection}}`),
-                    },
-                    {
-                        text: Localization.toText('editor-insert-link'), 
-                        icon: LinkIcon, 
-                        action: () => insertText(target, `\\link[] {${selection}}`),
-                    },
-                    {
-                        text: Localization.toText('editor-insert-linkTitle'), 
-                        icon: LinkIcon, 
-                        action: () => insertText(target, `\\linkTitle[]`),
-                    },
-                    {
-                        text: Localization.toText('editor-insert-linkContent'), 
-                        icon: LinkIcon, 
-                        action: () => insertText(target, `\\linkContent[]`),
+                        text: Localization.toText('editor-insert-interactive'),
+                        icon: InteractiveIcon,
+                        content: [
+                            {
+                                text: Localization.toText('editor-insert-link'), 
+                                icon: LinkIcon, 
+                                action: () => insertText(target, `\\link[] {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-linkTitle'), 
+                                icon: LinkIcon, 
+                                action: () => insertText(target, `\\linkTitle[]`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-linkContent'), 
+                                icon: LinkIcon, 
+                                action: () => insertText(target, `\\linkContent[]`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-roll'), 
+                                icon: DiceIcon, 
+                                action: () => insertText(target, `\\roll[20, num: 1, mod: 0, mode: dice, desc: ..., tooltips: ...] {${selection}}`),
+                            },
+                            {
+                                text: Localization.toText('editor-insert-save'), 
+                                icon: DiceIcon, 
+                                action: () => insertText(target, `\\save[10, type:temp, tooltips: ...]`),
+                            }
+                        ]
                     }
                 ]
             },
